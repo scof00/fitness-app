e7de641835fa6f3d0d9e2f5eef3f4a8481420cc0
scof00 <scofprofessional@gmail.com>
2024-05-09 09:58:34 -0400

    First Draft of exercises form

--- scripts/exercisesList.js
+++ scripts/exercisesList.js
@@ -9,7 +9,7 @@ export const muscleOptions = async () => {
     const response = await fetch("http://localhost:8088/muscles")
     const muscles = await response.json()
     document.addEventListener("change", handleSetMuscleChoice)
-    let muscleChoicesHTML = `<h4>Muscle Group</h4>`
+    let muscleChoicesHTML = `<label>Muscle Group</label>`
     muscleChoicesHTML += `<select id="muscle">
     <option value= "0">Choose muscle</option>`
     const divStringArray = await muscles.map(
@@ -19,5 +19,21 @@ export const muscleOptions = async () => {
     )
     muscleChoicesHTML+= divStringArray.join("")
     muscleChoicesHTML+= `</select>`
+    muscleChoicesHTML+= `<form>
+    <label>Exercise</label>
+    <input type="text" placeholder="Name of Exercise">
+    <label>Sets</label>
+    <input type="number" placeholder="# of Sets" min="0">
+    <label>Repetitions</label>
+    <input type="number" placeholder="# of Reps" min="0">
+    <label>Weight</label>
+    <input type="number" placeholder="Weight in kg or lbs" min="0">
+    </form>
+    <select>
+    <option value=1>Lbs</option>
+    <option value=2>Kgs</option>
+    </select>
+    <button>Save Exercise</button>
+    `
     return muscleChoicesHTML
 }
